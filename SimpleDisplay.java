7049a7e458922ce46362c7e79f40e991b1ee2879
jineshkjose <jineshkjose@gmail.com>
2022-06-04 01:05:16 +0530

    notes added

--- SimpleDisplay.java
+++ SimpleDisplay.java
@@ -2,11 +2,19 @@ public class SimpleDisplay
 {
 
 public static void main(String args[]){
+int b;
+b=10;
 
 
+System.out.println(" size of int in Java is (in bytes) :" + sizeof(int.class));
+
+System.out.println(++b);
+System.out.println(b);
 
 System.out.println("10");
 System.out.println("Jinesh");
+
+
 
 
 }
